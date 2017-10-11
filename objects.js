18b69b3a9916d51347fe906dff101a837b5cf04e
nathancoblentz <nathancoblentz@gmail.com>
2017-10-11 11:25:18 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,13 +1,13 @@
 var playlist = new Object ({
-    
+
       gratefulDead:"Ripple",
       Phish: "Piper",
       theBand: "Cripple Creek",
       tomPetty: "American Girl",
       prince: "Purple Rain"
-      
+
   })
-  
+
 
 function updatePlaylist(playlist, artistName, songTitle) {
   artistName="Phil Ochs"
