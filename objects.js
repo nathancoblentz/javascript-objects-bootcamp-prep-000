fd5b4fe35e4747b3bc4e97ebbeaf96daac3eafdf
nathancoblentz <nathancoblentz@gmail.com>
2017-10-11 11:08:31 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,12 @@
+var playlist =   {
+    gratefulDead:"Ripple",
+    Phish: "Piper",
+    theBand: "Cripple Creek",
+    tomPetty: "American Girl",
+    prince: "Purple Rain",
+  };
+
+  function updatePlaylist (playlist, artistName, songTitle) {
+  Object.assign({{}, {"Phil Ochs": "The Great State of Mississippi"}})
+    return playlist
+  }
