00c41a539267d7735a74787f02e5d320d07460bf
nathancoblentz <nathancoblentz@gmail.com>
2017-10-11 11:12:18 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,10 +1,13 @@
-var playlist =   {
-    gratefulDead:"Ripple",
-    Phish: "Piper",
-    theBand: "Cripple Creek",
-    tomPetty: "American Girl",
-    prince: "Purple Rain",
-  };
+var playlist =  new Object( 
+  {
+      gratefulDead:"Ripple",
+      Phish: "Piper",
+      theBand: "Cripple Creek",
+      tomPetty: "American Girl",
+      prince: "Purple Rain",
+    }
+  }
+)
 
   function updatePlaylist (playlist, artistName, songTitle) {
   Object.assign({{}, {"Phil Ochs": "The Great State of Mississippi"}})
